e21f16d85af3100a6b07a0d1294b166729f985b2
Amr <amr.mohamed.abdelwahab@gmail.com>
2016-05-29 23:30:29 +0200

    More styling to searchbar

--- src/components/searchbar.jsx
+++ src/components/searchbar.jsx
@@ -6,8 +6,8 @@ export default class Searchbar extends React.Component {
   render() {
     return (
       <div className='searchbar'>
-          <span className='ion-search seach-icon'/>
         <input className='search' placeholder='Search...'/>
+        <span className='ion-search seach-icon'/>
       </div>
     );
   }
