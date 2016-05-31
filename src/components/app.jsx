4078c10c3c7e988616e19b7ae146e1df4e8fd1f2
Amr <amr.mohamed.abdelwahab@gmail.com>
2016-05-31 19:30:11 +0200

    Implement the simple search functionality
    * Add the keyword to the app component state
    * Implement the getResult function that uses the keyword and books objects to define the result set
    * Implement the onChange function on the search Component
    * Implement the helper to trim and lower case the words to prepare it for search

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import Helpers from '../helpers/main.js'
+
 import Header from './header.jsx';
 import Bookshelf from './bookshelf.jsx';
 import Searchbar from './searchbar.jsx';
@@ -11,9 +13,9 @@ export default class App extends React.Component {
 
     this.state = {
       books: {},
-      tags: ['ruby', 'design']
+      tags: ['ruby', 'design'],
+      keyword:''
     };
-    console.log(this.state.tags);
   }
 
   saveBook(book) {
@@ -22,13 +24,41 @@ export default class App extends React.Component {
     this.setState({books: this.state.books})
   }
 
+  updateKeyword(keyword) {
+    this.setState({keyword: keyword});
+  }
+
+  getResults() {
+    var book_ids = Object.keys(this.state.books);
+    var keyword = Helpers.prepareForSearch(this.state.keyword);
+    var result_ids = book_ids.filter(
+      book_id =>
+        Helpers.prepareForSearch(this.state.books[book_id].author)
+          .includes(keyword) ||
+        Helpers.prepareForSearch(this.state.books[book_id].title)
+          .includes(keyword)  ||
+        this.state.books[book_id].tags.filter(
+          tag => 
+            Helpers.prepareForSearch(tag)
+              .includes(keyword)
+              ).length > 0
+      );
+
+    var results = {};
+    for (var id of result_ids) {
+     results[id] = this.state.books[id];
+    }
+    
+    return results;
+  }
+
   render() {
     return (
      <div className='app'>
       <Header/>
        <div className='main-panel'>
-        <Searchbar/>
-        <Bookshelf saveBook={this.saveBook.bind(this)} books={this.state.books}/>
+        <Searchbar updateKeyword={this.updateKeyword.bind(this)}/>
+        <Bookshelf saveBook={this.saveBook.bind(this)} books={this.getResults()}/>
       </div>
      </div>
       )
--- src/components/searchbar.jsx
+++ src/components/searchbar.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 
 export default class Searchbar extends React.Component {
- 
+  
+  changeKeyword(){
+    var keyword = this.refs.keyword.value;
+    this.props.updateKeyword(keyword);
+  }
 
   render() {
     return (
       <div className='searchbar'>
-        <input className='search' placeholder='Search...'/>
+        <input className='search' ref='keyword' onChange={this.changeKeyword.bind(this)} placeholder='Search...'/>
         <span className='ion-search seach-icon'/>
       </div>
     );
