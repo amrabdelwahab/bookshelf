a6acd5538d3ceddd1b2de708588e084cdf4d2db5
Amr <amr.mohamed.abdelwahab@gmail.com>
2016-06-01 10:05:27 +0200

    Add react paginate, configure it and style it to enable pagination
    * install react paginate and its dependencies
    * in the state of bookshelf have 4 different attributes that binds to the pagination data, page, pageNum and offset
    * in the props of bookshelf pass books with the entire set of filtered books and perPage
    * style the pagination bar

--- src/components/bookshelf.jsx
+++ src/components/bookshelf.jsx
@@ -1,18 +1,90 @@
 import React from 'react';
+import ReactPaginate from 'react-paginate';
 
 import Book from './book.jsx';
 import AddBookForm from './addbookform.jsx'
 
 export default class Bookshelf extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      offset: 0,
+      page: 0
+    }
+  }
+
+  loadBooks(books) {
+    const { offset } = this.state;
+    var books_ids = Object.keys(books).reverse();
+    var results = {};
+    for(var i= offset; (i < books_ids.length) && (i < (offset+this.props.perPage)); i++)
+      results[books_ids[i]] = books[books_ids[i]];
+
+    return(results);
+  }
+
+  componentDidMount() {
+    var books_ids = Object.keys(this.props.books);
+    this.setState({
+      pageNum: Math.ceil(books_ids.length / this.props.perPage),
+      data: this.loadBooks(this.props.books)
+    });
+  }
+
+  componentWillReceiveProps(nextProps) {
+    var books_ids = Object.keys(nextProps.books);
+    let selected = 0;
+    let offset = Math.ceil(selected * this.props.perPage);
+    this.setState({ 
+      page: 0,
+      offset:0, 
+      pageNum: Math.ceil(books_ids.length / this.props.perPage),
+      data: this.loadBooks(nextProps.books) 
+    });
+  }
+
+  handlePageClick(event) {
+    let selected = event.selected;
+    let offset = Math.ceil(selected * this.props.perPage);
+    this.setState({
+      offset: offset,
+      page: selected
+    });
+    this.setState({
+      data: this.loadBooks(this.props.books)
+    });
+  };
+
+
   renderBook(key){
     return <Book key={key} details={this.props.books[key]} />
   }
+
   render() {
     return (
-      <ul className='bookshelf'>
-        <AddBookForm {...this.props}/>
-        {Object.keys(this.props.books).reverse().map(this.renderBook.bind(this))}
-      </ul>
+      <div>
+        <ul className='bookshelf'>
+          <AddBookForm {...this.props}/>
+          {
+            Object.
+              keys(this.loadBooks(this.props.books)).
+              map(this.renderBook.bind(this))
+            }
+        </ul>
+        <ReactPaginate previousLabel={"<"}
+                         nextLabel={">"}
+                         breakLabel={<a href="">...</a>}
+                         pageNum={this.state.pageNum}
+                         marginPagesDisplayed={2}
+                         pageRangeDisplayed={5}
+                         clickCallback={this.handlePageClick.bind(this)}
+                         containerClassName={"pagination"}
+                         subContainerClassName={"pages pagination"}
+                         activeClassName={"active"}
+                         initialSelected={0}
+                         forceSelected={this.state.page} />
+      </div>
     )
   }
 }
